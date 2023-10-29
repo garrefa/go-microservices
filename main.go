7bb7378a4217bc26af1d780f37ab54208b182c34
Alex Garrefa <alexmrg@gmail.com>
2023-10-29 15:51:29 +0100

    Episode 01 content

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
+		log.Println("Hello world!")
+		data, err := io.ReadAll(req.Body)
+		if err != nil {
+			http.Error(rw, "Ooooops", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(rw, "Hello %s", data)
+	})
+
+	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Goodbye!")
+	})
+
 	http.ListenAndServe("127.0.0.1:8080", nil)
 }
